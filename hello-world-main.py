4fd792ee2c1e6df4c599d8b819356f803db421b3
aphmorrissey <apmorrissey@smith.edu>
2025-01-31 16:35:01 +0000

    tested and done

--- hello-world-main.py
+++ hello-world-main.py
@@ -4,3 +4,9 @@
 
 # The line show below is a python instruction that will be executed
 print("hello world!")
+
+username = input("What should I call you?")
+your_years = int(input("How many years are you?"))
+sun_miles = 584000000 * your_years
+print("Hello" , username)
+print("You have traveled roughly" , sun_miles , " miles around the sun!")
